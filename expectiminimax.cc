920b1438a5ca6537496c09ea92404273047f5f6b
paulpak58 <paulpak58@gmail.com>
2022-12-14 17:02:40 -0600

    Minimax and expected minimax search policies and updated configurations on the game state

--- expectiminimax.cc
+++ expectiminimax.cc
@@ -7,64 +7,60 @@ using namespace std;
 namespace open_spiel{
 namespace algorithms {
 namespace {
-	/*	Minmax alpha-beta algorithms
+	/*	Expectiminimax algorithm
 	 *	Args:
 	 *		state: current game state
 	 *		depth: max depth for min/max search
-	 *		alpha: best value that max player can guarantee
-	 *		beta: best value that min player can guarantee
 	 *		value_function: maps Spiel 'State' to value
 	 *		maximizing_player_id: id of MAX
 	 *	Returns:
 	 *		Optimal value of sub-game starting in initial state
 	 */
-double _alpha_beta(State* state, int depth, double alpha, double bea,function<double(const State&)> value_function,Player maximizing_player,Action* best_action){
-	if(state->IsTerminal()){
-		return state->PlayerReturn(maximizing_player);
-	}
+double _expectiminimax(const State* state,int depth,function<double(const State&)> value_function,Player maximizing_player,Action* beset_action){
+	if(state->IsTerminal()) return state->PlayerReturn(maximizing_player);
 	if(depth==0) return value_function(*state);
-
 	Player player = state->CurrentPlayer();
-	if(player==maximizing_player){
-		// Default value at each state
-		double value = -numeric_limits<double>infinity();
+	if(state->IsChanceNode()){
+		double value=0;
+		for(const auto& p: state->ChanceOutcomes()){
+			unique_ptr<State> child_state = state->Child(p.first);
+			double child_value = _expectiminimax(child_state.get(), depth,value_function,maximizing_player,nullptr);
+			value += p*child_value;
+		}
+		return value;
+	}else if(player==maximizing_player){
+		double value = -numeric_limits<double>
 		for(Action action:state->LegalActions()){
-			state->ApplyAction(action);
-			double child_value=_alpha_beta(state,depth-1,alpha,beta,value_function,maximizing_player,nullptr);
-			state->UndoAction(player,action);
+			unique_ptr<State> child_state = state->Child(action);
+			double child_value=_expectiminimax(child_state.get(),depth-1,value_function,maximizing_player,nullptr);
 			if(child_value>value){
 				value = child_value;
 				if(best_action!=nullptr) 
 					*best_action=action;
 			}
-			alpha = max(alpha,value);
-			if(alpha>=beta) break;
 		}
 		return value;
-	}else{			// SECOND player
-		double value = numeric_limits<double>::infinity();
-		for(Action action: state->LegalActions()){
-			state->ApplyAction(action);
-			double child_value=_alpha_beta(state,depth-1,alpha,beta,value_function,maximizing_player,nullptr);
-			state->UndoAction(player,action);
+	}else{
+		double value = numeric_limits<double>
+		for(Action action:state->LegalActions()){
+			unique_ptr<State> child_state = state->Child(action);
+			double child_value=_expectiminimax(child_state.get(),depth-1,value_function,maximizing_player,nullptr);
 			if(child_value<value){
 				value = child_value;
 				if(best_action!=nullptr) 
 					*best_action=action;
 			}
-			beta = min(beta,value);
-			if(alpha>=beta) break;
 		}
 		return value;
 	}
 }
 
-pair<double,action> alphabetasearch(const game& game,const state* state,function<double(const state&)> value_function,int depth_limit,player maximizing_player){
+pair<double,action> ExpectiminimaxSearch(const game& game,const state* state,function<double(const state&)> value_function,int depth_limit,player maximizing_player){
 	// minmax played between two players
 	// set configurations for the game state
 	SPIEL_CHECK_LE(game.numplayers(),2);
 	GameType game_info = game.GetType();
-	SPIEL_CHECK_EQ(game_info.chance_mode,GameType::ChanceMode::kDeterministic);
+	SPIEL_CHECK_EQ(game_info.chance_mode,GameType::ChanceMode::kExplicitStochastic);
 	SPIEL_CHECK_EQ(game_info.information,GameType::Information::kPerfectInformation);
 	SPIEL_CHECK_EQ(game_info.dynamics,GameType::Dynamics::kSequential);
 	SPIEL_CHECK_EQ(game_info.utility,GameType::Utility::kZeroSum);
@@ -75,14 +71,15 @@ pair<double,action> alphabetasearch(const game& game,const state* state,function
 	else
 		search_root = state->Clone();
 	if(maximizing_player==kInvalidPlayer){
+		SPIEL_CHECK_FALSE(search_root->IsChanceNode());
 		maximizing_player = search_root->CurrentPlayer();
 	}
-	double infinity = numeric_limits<double>infinity();
 	Action best_action = kInvalidAction;
-	double value = _alpha_beta(search_root.get(),depth_limit,-infinity,infinity,value_function,maximizing_player,&best_action);
+	double value = _expectiminimax(search_root.get(),depth_limit,value_function,maximizing_player,&best_action);
 	return {value,best_action};
 }
 
+
 }	// namespace
-}	// algorithms
+}	// algorithm
 }	// open_spiel
